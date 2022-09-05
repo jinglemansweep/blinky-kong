9dadf1b291ed61746d70ef5906863ac6261ad385
JingleManSweep <jinglemansweep@gmail.com>
2022-09-05 22:34:56 +0000

    More initial commit

--- src/__main__.py
+++ src/__main__.py
@@ -1,26 +1,39 @@
+import time
 from .utils import build_strip, render_strip_led, render_strip_text, send_wled_udp
 
 WLED_HOST = "10.0.2.86"
 WLED_PORT = 21324
 
+STRIP_ENABLED = True
+STRIP_UPDATE_DELAY = 0.01
 STRIP_LENGTH = 300
 STRIP_INVERT = True
 MAP_WIDTH = 100
 MAP_OFFSET = 20
 
-strip = build_strip(
-    strip_length=STRIP_LENGTH, 
-    strip_invert=STRIP_INVERT, 
-    map_width=MAP_WIDTH, 
-    map_offset=MAP_OFFSET
-)
+CHAR_X_START = 1
 
-rendered = render_strip_text(strip)
-led_bytes = render_strip_led(strip)
+char_x = CHAR_X_START
 
-print("TEXT")
-print(rendered)
-#print("LED BYTES")
-#print(led_bytes)
+while True:
 
-send_wled_udp(WLED_HOST, WLED_PORT, led_bytes)
+    strip = build_strip(
+        strip_length=STRIP_LENGTH, 
+        strip_invert=STRIP_INVERT, 
+        map_width=MAP_WIDTH, 
+        map_offset=MAP_OFFSET,
+        char_x=char_x
+    )
+
+    rendered = render_strip_text(strip)
+    led_bytes = render_strip_led(strip)
+
+    print(rendered)
+
+    char_x += 1
+    if (char_x >= 80): char_x = CHAR_X_START
+
+    if STRIP_ENABLED:
+        send_wled_udp(WLED_HOST, WLED_PORT, led_bytes, timeout=1)
+
+    time.sleep(STRIP_UPDATE_DELAY)
--- src/utils.py
+++ src/utils.py
@@ -9,31 +9,38 @@ STRIP_TIMEOUT = 5
 STRIP_MARKER_EMPTY = 0
 STRIP_MARKER_START = 10
 STRIP_MARKER_END = 20
+STRIP_MARKER_CHAR = 30
 
 STRIP_RENDER_TEXT_EMPTY = "_"
 STRIP_RENDER_TEXT_START = "["
 STRIP_RENDER_TEXT_END = "]"
+STRIP_RENDER_TEXT_CHAR = "@"
 
-STRIP_RENDER_LED_EMPTY = (0, 0, 0)   # off
-STRIP_RENDER_LED_START = (0, 255, 0) # red
-STRIP_RENDER_LED_END = (255, 0, 0)   # blue
+STRIP_RENDER_LED_EMPTY = (0, 0, 0)      # off
+STRIP_RENDER_LED_START = (0, 255, 0)    # red
+STRIP_RENDER_LED_END = (255, 0, 0)      # blue
+STRIP_RENDER_LED_CHAR = (255, 255, 255) # white
 
 MAP_WIDTH = 80
 MAP_OFFSET = 10
+CHAR_X = int(MAP_WIDTH / 2)
 
 def get_x(x, width, invert=False):
     return (width - x if invert else x) - 1
 
-def build_strip(strip_length=STRIP_LENGTH, strip_invert=STRIP_INVERT, map_width=MAP_WIDTH, map_offset=MAP_OFFSET):
-    idx_start = 0 + map_offset
-    idx_end = idx_start + map_width
+def build_strip(strip_length=STRIP_LENGTH, strip_invert=STRIP_INVERT, map_width=MAP_WIDTH, map_offset=MAP_OFFSET, char_x=CHAR_X):
+    map_start_x = 0 + map_offset
+    map_end_x = map_start_x + map_width
+    # Set X locations of Map Start/End and Character
+    x_start = get_x(map_start_x, strip_length, strip_invert)
+    x_end = get_x(map_end_x, strip_length, strip_invert)
+    x_char = get_x(map_start_x + char_x, strip_length, strip_invert)
+    # Clear strip
     strip = [STRIP_MARKER_EMPTY for i in range(0, strip_length)]
-    # set map start
-    start_x = get_x(idx_start, strip_length, strip_invert)
-    print("start x", start_x)
-    strip[get_x(idx_start, strip_length, strip_invert)] = STRIP_MARKER_END if strip_invert else STRIP_MARKER_START
-    # set map end
-    strip[get_x(idx_end, strip_length, strip_invert)] = STRIP_MARKER_START if strip_invert else STRIP_MARKER_END
+    # Populate map array
+    strip[x_start] = STRIP_MARKER_END if strip_invert else STRIP_MARKER_START
+    strip[x_end] = STRIP_MARKER_START if strip_invert else STRIP_MARKER_END
+    strip[x_char] = STRIP_MARKER_CHAR
     return strip
 
 def render_strip_text(strip):
@@ -43,6 +50,8 @@ def render_strip_text(strip):
             out.append(STRIP_RENDER_TEXT_START)
         elif i == STRIP_MARKER_END:
             out.append(STRIP_RENDER_TEXT_END)
+        elif i == STRIP_MARKER_CHAR:
+            out.append(STRIP_RENDER_TEXT_CHAR)
         elif i == STRIP_MARKER_EMPTY:
             out.append(STRIP_RENDER_TEXT_EMPTY)
     return "".join(out)
@@ -54,6 +63,8 @@ def render_strip_led(strip):
             out.append(STRIP_RENDER_LED_START)
         elif i == STRIP_MARKER_END:
             out.append(STRIP_RENDER_LED_END)
+        elif i == STRIP_MARKER_CHAR:
+            out.append(STRIP_RENDER_LED_CHAR)
         elif i == STRIP_MARKER_EMPTY:
             out.append(STRIP_RENDER_LED_EMPTY)
     return out
